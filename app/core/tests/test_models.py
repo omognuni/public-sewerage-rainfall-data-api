6290bb110be741797b413062bfc854f324fce0fe
omognuni <melbong0069@gmail.com>
2022-11-10 17:01:02 +0900

    [Test] 모델 테스트

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,66 +1,32 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from core.models import Record
+from datetime import datetime
 
-
-def create_user(email='user@example.com', password='testpass123'):
-    '''user 생성'''
-    return get_user_model().objects.create_user(email, password)
+def create_user(username='testname', password='testpass'):
+    return get_user_model().objects.create_user(username, password)
 
 class ModelTest(TestCase):
-    '''모델 생성 테스트'''
+    '''모델 생성 테스트'''    
     
     def test_create_user_model(self):
-        '''새로운 유저 생성'''
-        email = 'test@gmail.com'
+        '''유저 모델 생성 테스트'''
+        username = 'testname'
         password = 'testpass'
-        user = get_user_model().objects.create_user(email=email,
-                                                    password=password)
-
-        self.assertEqual(email, user.email)
+        user = get_user_model().objects.create_user(username=username, password=password)
+        
+        self.assertEqual(user.username, username)
         self.assertTrue(user.check_password(password))
-
-    def test_noramilize_email(self):
-        '''이메일 소문자 변환'''
-        email = 'test@GMAIL.COM'
-        password = 'testpass'
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
-
-        self.assertEqual(email.lower(), user.email)
-
-    def test_email_validation(self):
-        '''이메일 없이 가입 시 에러'''
-        with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(
-                '',
-                password='testpass'
-            )
-
+        
     def test_create_superuser(self):
-        '''superuser 생성'''
-        email = 'test@gmail.com'
+        '''superuser 생성 테스트'''
+        username = 'testname'
         password = 'testpass'
-        user = get_user_model().objects.create_superuser(
-            email=email,
-            password=password
-        )
-
-        self.assertTrue(user.is_staff)
+        
+        user = get_user_model().objects.create_superuser(username=username, password=password)
+        
+        self.assertEqual(user.username, username)
+        self.assertTrue(user.check_password(password))
         self.assertTrue(user.is_superuser)
-        
-    def test_create_book(self):
-        '''가계부 내역 생성'''
-        user = create_user()
-        amount = 10000
-        category = 'cash'
-        memo = 'test memo'
-        record = Record.objects.create(user=user, category=category, amount=amount, memo=memo)
-        
-        self.assertEqual(record.user, user)
-        self.assertEqual(record.amount, amount)
-        
-        
+        self.assertTrue(user.is_staff)
+    
