e8befb9a8a388bc0ec8ec4e4b949366aeba93b02
omognuni <melbong0069@gmail.com>
2022-11-10 17:00:30 +0900

    [Chore] 설정 세팅

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,6 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
+OPENAPI_KEY = os.environ.get('OPENAPI_KEY', 'openapikey')
+
+OPENAPI_URL = os.environ.get('OPENAPI_URL', 'openapiurl') + OPENAPI_KEY + '/'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'core',
+    'user',
+    'fetch',
 ]
 
 MIDDLEWARE = [
